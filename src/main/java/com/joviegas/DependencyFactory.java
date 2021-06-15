f02a52f8c9a113c5717b438689b102c8b20c633b
John Viegas <maxsjohn@gmail.com>
2021-06-15 08:26:55 -0700

    Adding baseline files

---
+++ src/main/java/com/joviegas/DependencyFactory.java
@@ -0,0 +1,27 @@
+
+package com.joviegas;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.core.SdkSystemSetting;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+/**
+ * The module containing all dependencies required by the {@link App}.
+ */
+public class DependencyFactory {
+
+    private DependencyFactory() {}
+
+    /**
+     * @return an instance of S3Client
+     */
+    public static S3Client s3Client() {
+        return S3Client.builder()
+                       .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                       .region(Region.of(System.getenv(SdkSystemSetting.AWS_REGION.environmentVariable())))
+                       .httpClientBuilder(UrlConnectionHttpClient.builder())
+                       .build();
+    }
+}
